8dc7ff40a6ed697fc4fea2bb7ecbddba607c3409
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-18 13:12:16 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,15 @@
+# vite-plugin-remote-assets
+
+[![NPM version](https://img.shields.io/npm/v/vite-plugin-remote-assets?color=a1b858&label=)](https://www.npmjs.com/package/vite-plugin-remote-assets)
+
+## Sponsors
+
+<p align="center">
+  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
+    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
+  </a>
+</p>
+
+## License
+
+[MIT](./LICENSE) License © 2021 [Anthony Fu](https://github.com/antfu)
---
+++ src/index.ts
@@ -0,0 +1,120 @@
+import { extname, resolve, join } from 'path'
+import { existsSync, mkdirSync, createWriteStream } from 'fs'
+import http from 'http'
+import https from 'https'
+import _debug from 'debug'
+import md5 from 'blueimp-md5'
+import { Plugin } from 'vite'
+
+export interface RemoteAssetsRule {
+  match: RegExp
+  ext?: string
+}
+
+export interface RemoteAssetsOptions {
+  /**
+   * Directory name to store the assets from remote
+   *
+   * @default '.remote-assets'
+   */
+  assetsDir?: string
+
+  /**
+   * Rules to match urls to replace
+   */
+  rules?: RemoteAssetsRule[]
+}
+
+export const DefaultRules: RemoteAssetsRule[] = [
+  {
+    match: /\b(https?:\/\/[\w_#&?.\/-]*?\.(?:png|jpe?g|svg|ico))(?=[`'")\]])/ig,
+  },
+]
+
+function isValidHttpUrl(str: string) {
+  let url
+  try {
+    url = new URL(str)
+  }
+  catch (_) {
+    return false
+  }
+  return url.protocol === 'http:' || url.protocol === 'https:'
+}
+
+const debug = _debug('vite-plugin-remote-assets')
+
+export function VitePluginRemoteAssets(options: RemoteAssetsOptions = {}): Plugin {
+  const {
+    assetsDir = '.remote-assets',
+    rules = DefaultRules,
+  } = options
+
+  let dir: string = undefined!
+
+  async function downloadTo(url: string, filepath: string): Promise<void> {
+    const file = createWriteStream(filepath)
+    const client = url.startsWith('https') ? https : http
+    const request = client.get(url, res => res.pipe(file))
+
+    return new Promise((resolve, reject) => {
+      request.on('finish', () => {
+        file.close()
+        resolve()
+      })
+      request.on('error', (e) => {
+        file.destroy()
+        reject(e)
+      })
+    })
+  }
+
+  return {
+    name: 'vite-plugin-remote-assets',
+    enforce: 'pre',
+    configResolved(config) {
+      dir = resolve(config.publicDir, assetsDir)
+      if (!existsSync(dir))
+        mkdirSync(dir)
+    },
+    async transform(code) {
+      let matched = false
+      const tasks: Promise<void>[] = []
+
+      for (const rule of rules) {
+        code = code.replace(
+          rule.match,
+          (full, url) => {
+            url = url || full
+            if (!url || !isValidHttpUrl(url))
+              return full
+
+            matched = true
+            const hash = md5(url) + rule.ext || extname(url)
+            const filepath = join(dir, hash)
+
+            debug('detected', url, hash)
+
+            if (!existsSync(filepath)) {
+              tasks.push((async() => {
+                debug('downloading', url)
+                await downloadTo(url, filepath)
+                debug('downloaded', url)
+              })())
+            }
+
+            return `/${assetsDir}/${hash}`
+          },
+        )
+      }
+
+      if (!matched)
+        return
+
+      if (tasks.length)
+        await Promise.all(tasks)
+
+      return code
+    },
+  }
+}
